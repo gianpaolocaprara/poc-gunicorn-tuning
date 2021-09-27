0b638da3597aec8a99b609af74b40754e4397055
Gianpaolo Caprara <gianpaolo.caprara@chili.com>
2021-09-27 16:24:54 +0200

    Init commit

---
+++ flask_app/app.py
@@ -0,0 +1,15 @@
+# ./flask_app/app.py
+import os
+import requests
+from flask import Flask, request
+
+api_port = os.environ['PORT_API']
+api_url = f'http://slow_api:{api_port}/'
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    delay = float(request.args.get('delay') or 1)
+    resp = requests.get(f'{api_url}?delay={delay}')
+    return 'Hi there! ' + resp.text
---
+++ flask_app/patched.py
@@ -0,0 +1,5 @@
+# ./flask_app/patched.py
+from gevent import monkey
+monkey.patch_all() # we need to patch very early
+
+from app import app  # re-export
