e2bf4a69028996b181e5c1c3840d9be15a1ac313
francesco_luppino <francesco.luppino@chili.com>
2021-09-29 15:07:20 +0200

    Added report

--- flask_app/sql_app_patched.py
+++ flask_app/sql_app_patched.py
@@ -1,8 +1,5 @@
-from gevent import monkey
-
-monkey.patch_all()  # we need to patch very early
+# from gevent import monkey
+# monkey.patch_all()  # we need to patch very early
 import psycogreen.gevent
-
 psycogreen.gevent.patch_psycopg()
-
 from sql_app import app
